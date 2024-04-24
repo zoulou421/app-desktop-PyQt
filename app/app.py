078bdb7c6bed3352b6bf876fca8fd305bcc1391f
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-24 02:08:37 +0200

    added: create and add widgets

--- app/app.py
+++ app/app.py
@@ -6,6 +6,21 @@ class App(QtWidgets.QWidget):
         # QtWidgets.QWidget.__init__()
         super().__init__()
         self.setWindowTitle("Currency Converter")
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.layout = QtWidgets.QHBoxLayout(self)
+        self.ccb_currency_from = QtWidgets.QComboBox()
+        self.spn_amount = QtWidgets.QSpinBox()
+        self.ccb_currency_to = QtWidgets.QComboBox()
+        self.spn_amount_converted = QtWidgets.QSpinBox()
+        self.btn_inverse = QtWidgets.QPushButton("Inverse Currency")
+
+        self.layout.addWidget(self.ccb_currency_from)
+        self.layout.addWidget(self.spn_amount)
+        self.layout.addWidget(self.ccb_currency_to)
+        self.layout.addWidget(self.spn_amount_converted)
+        self.layout.addWidget(self.btn_inverse)
 
 
 app = QtWidgets.QApplication([])
