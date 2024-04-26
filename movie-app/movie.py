c56c2fec8534059d740252852a18222eaa509856
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-27 00:47:24 +0200

    added: create _get and _write methods

--- movie-app/movie.py
+++ movie-app/movie.py
@@ -1,3 +1,10 @@
+import os
+import json
+
+CUR_DIR = os.path.dirname(__file__)
+DATA_FILE = os.path.join(CUR_DIR, "data", "movies.json")
+
+
 class Movie:
     def __init__(self, movie_title):
         self.movie_title = movie_title.title()
@@ -5,8 +12,15 @@ class Movie:
     def __str__(self):
         return self.movie_title
 
+    def _get_movies(self):
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+
+    def _write_movies(self, list_movies):
+        with open(DATA_FILE, "w") as f:
+            json.dump(list_movies, f, indent=4)
+
 
 if __name__ == "__main__":
-    m = Movie("Jean Claude Vandamme")
-    print(m)
-
+    m = Movie("Formationkilo movie")
+    m._write_movies(["Moses movie1", "Court movie 1"])
